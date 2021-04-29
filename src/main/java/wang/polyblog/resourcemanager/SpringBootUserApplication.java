1daf8fa2c32b90481aa5b39a787375cbe645c50b
Poly1316 <wangxiaotao1316@gmail.com>
2021-04-29 20:51:11 +0800

    单元测试问题修改

--- src/main/java/wang/polyblog/resourcemanager/SpringBootUserApplication.java
+++ src/main/java/wang/polyblog/resourcemanager/SpringBootUserApplication.java
@@ -3,7 +3,9 @@ package wang.polyblog.resourcemanager;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -12,9 +14,8 @@ import org.springframework.scheduling.annotation.EnableScheduling;
  * Created by User on 2020/7/6.
  */
 
-//@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
-@SpringBootApplication
-@MapperScan("cn.org.dao")
+@SpringBootApplication()
+@MapperScan("wang.polyblog.resourcemanager.dao")
 @EnableScheduling
 public class SpringBootUserApplication extends SpringBootServletInitializer {
 
